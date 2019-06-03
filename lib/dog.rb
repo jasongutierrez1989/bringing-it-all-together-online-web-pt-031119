1ff6f4447e67f2e59be1fc7587d2fe1582789b98
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-03 05:44:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,13 +34,6 @@ class Dog
     DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     self
-    #dog = Dog.new
-
-    #dog.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-    #dog.name = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][1]
-    #dog.breed = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][2]
-
-    #dog
   end
 
 
