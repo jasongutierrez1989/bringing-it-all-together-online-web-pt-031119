9b369bf2407da98d4ad2a6d925b16fa5dade34ec
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-03 05:35:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,14 +27,6 @@ class Dog
 
   def save
     sql = <<-SQL
-      INSERT INTO dogs (name, breed)
-      VALUES (?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-    def save
-    sql = <<-SQL
       INSERT INTO songs (name, album)
       VALUES (?, ?)
     SQL
